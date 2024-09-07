2eaeb404c64618a064051101146cde8c4b8fe4b5
radlee <prof.lolito@gmail.com>
2024-09-07 23:18:19 +0200

    Fixed Production issues

--- app.py
+++ app.py
@@ -308,5 +308,7 @@ def index():
     ''', playlists=playlists)
 
 if __name__ == '__main__':
-    # Adjust the port and host as needed for deployment or local testing
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
